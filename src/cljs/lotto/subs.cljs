56a1942841350b1b296afceed1da05cdb7036ea1
Eric Normand <ericwnormand@gmail.com>
2016-10-11 23:15:04 -0500

    This is what people should start with.

--- src/cljs/lotto/subs.cljs
+++ src/cljs/lotto/subs.cljs
@@ -1,29 +1,3 @@
 (ns lotto.subs
     (:require-macros [reagent.ratom :refer [reaction]])
     (:require [re-frame.core :as re-frame]))
-
-(re-frame/reg-sub
- :app-state
- (fn [db]
-   db))
-
-(re-frame/reg-sub
- :grid-size
- (fn [db]
-   {:height (count (get db :cards))
-    :width (count (first (get db :cards)))}))
-
-(re-frame/reg-sub
- :card-at
- (fn [db [_ x y]]
-   (get-in db [:cards y x])))
-
-(re-frame/reg-sub
- :current-player
- (fn [db]
-   (get db :current-player)))
-
-(re-frame/reg-sub
- :scores
- (fn [db]
-   (get db :scores)))
--- src/cljs/lotto/views.cljs
+++ src/cljs/lotto/views.cljs
@@ -3,61 +3,6 @@
             [re-frame.core :as re-frame]
             [lotto.cards :as cards]))
 
-(defn card-face [tile]
-  [:div.front
-   [:div.card {:style {:font-size "240px"}}
-    (get cards/mahjong tile)]])
-
-(defn card-back []
-  [:div.back
-   [:div.card]])
-
-(defn oriented-card [card]
-  [:div {:class (if (cards/front? card)
-                  "flip-container"
-                  "flip-container back-of-card")}
-   (when card
-     [:div.flipper
-      [card-back]
-      [card-face (cards/face card)]])])
-
-(defn grid [f]
-  (let [grid-size (re-frame/subscribe [:grid-size])
-        height (:height @grid-size)
-        width (:width @grid-size)]
-    [:table
-     [:tbody
-      (doall
-       (for [y (range height)]
-         [:tr {:key (str y)}
-          (doall
-           (for [x (range width)]
-             [:td {:key (str x)}
-              [f x y]]))]))]]))
-
-(defn flipper [x y]
-  (let [card (re-frame/subscribe [:card-at x y])]
-    [:div {:on-click (fn [] (re-frame/dispatch [:flip-up x y]))}
-     [oriented-card @card]]))
-
 (defn main-panel []
-  (let [current-player (re-frame/subscribe [:current-player])
-        scores         (re-frame/subscribe [:scores])]
-    [:div
-     [:div.info
-      [:div
-       [:h2 "Current player: "]
-       (name @current-player)]
-      [:div
-       [:h2  "Scores: "]
-       (for [score-record @scores]
-         (let [player (first score-record)
-               score (second score-record)]
-           [:div {:key (name player)}
-            (name player) " " (str score)]))]]
-     [grid flipper]
-     [:div [:button {:on-click (fn []
-                                 (re-frame/dispatch
-                                  [:shuffle-cards 2 2 (keys cards/mahjong)]))}
-            "Shuffle."]]
-     [state-viewer]]))
+  [:div
+   "Welcome to the world's first New Orleans Clojure Workshop!"])
